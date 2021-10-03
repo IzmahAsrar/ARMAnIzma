7ac51f4b85e47a1711b552135315d1c1cbb0b98c
IzmahAsrar <85062164+IzmahAsrar@users.noreply.github.com>
2021-10-03 13:34:55 +0500

    MainSource Code
    ArCore,Gameplay is done in this package

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab;
+    public GameObject PacmanController;
+    int x = 0;
+    public float TimeaLimit;
+    public float h, s;
+    public int currentlevel=1;
+    void Start()
+    {
+        if (currentlevel == 1)
+        {
+           
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (s > 0)
+        {
+            s -= 1 * Time.deltaTime;
+            // 
+        }
+        else
+        {
+            if (h > 0)
+            {
+                h--;
+                s = 60;
+            }
+           
+        }
+        if (h == 0 && s == 0)
+        {
+            print("Gameover");
+        }
+        //if (h > 0)
+        //{
+        //    s = 60;
+            
+        //}
+        if (GameObject.FindWithTag("SpawnGame") && x == 0)
+        {
+            PacmanController.GetComponent<PacmanController>().enabled = true;
+            Instantiate(prefab, GameObject.FindWithTag("SpawnGame").transform.position, GameObject.FindWithTag("SpawnGame").transform.rotation);
+            x = 1;
+        }
+    }
+}
---
+++ Assets/animscript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material Water;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Water.mainTextureOffset += new Vector2(0.5f*Time.deltaTime, 0);
+    }
+}
